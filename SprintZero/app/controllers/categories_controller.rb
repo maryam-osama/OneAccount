23ceedaa8a44f1aacc38e1fed4962f8b02c732a1
rawan_abdelhalim <rawan.abdelhalim2@gmail.com>
2015-03-30 15:43:20 +0000

    #25 add a new category (without the html code) and listing the categories added; work still in progress

---
+++ SprintZero/app/controllers/categories_controller.rb
@@ -0,0 +1,16 @@
+class CategoriesController < ApplicationController
+     
+     def new
+     end
+       def index
+    @categories = Category.all
+       end
+     def create
+         @category = Category.new(params.require(:category).permit(:name))
+          @category.save
+  redirect_to categories_path
+     end
+     def show
+         @category = Category.find(params[:id])
+     end
+end
--- SprintZero/db/schema.rb
+++ SprintZero/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150320125146) do
+ActiveRecord::Schema.define(version: 20150330145215) do
 
   create_table "applications", force: true do |t|
     t.string   "appname"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 20150320125146) do
     t.string   "publisher_email"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+  end
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "purchases", force: true do |t|
