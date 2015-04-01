bd2ab35873bf1f92e2c76131052a0db8c824a451
maryam_osama <maryam.osama06@gmail.com>
2015-04-01 23:42:06 +0000

    I added a global variable

--- SprintZero/db/schema.rb
+++ SprintZero/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150329214829) do
+ActiveRecord::Schema.define(version: 20150322204630) do
 
   create_table "applications", force: true do |t|
     t.string   "appname"
@@ -20,13 +20,6 @@ ActiveRecord::Schema.define(version: 20150329214829) do
     t.string   "publisher_email"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.string   "category_name"
-  end
-
-  create_table "categories", force: true do |t|
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   create_table "notifications", force: true do |t|
