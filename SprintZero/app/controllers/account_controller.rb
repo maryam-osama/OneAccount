45271c8ba93222d1e9aef85530ac52f92dcdd9d1
maikany <mai.kany62@gmail.com>
2015-04-02 22:40:53 +0000

    I modified the documentation and the coding style

--- SprintZero/app/controllers/account_controller.rb
+++ SprintZero/app/controllers/account_controller.rb
@@ -1,7 +1,4 @@
 class AccountController < ApplicationController
-    def signin
-        
-    end
-    
-    
+    def signin; end
+
 end
--- SprintZero/app/controllers/applications_controller.rb
+++ SprintZero/app/controllers/applications_controller.rb
@@ -1,34 +1,28 @@
 class ApplicationsController < ApplicationController
+    def new; end
     
-    
-    def new 
+    def index
+      if params[:search]
+       @applications = Application.search(params[:search]).order("created_at DESC")
+      end
     end
+   
+     # a method for viewing my purchases
+     def show
+     @application = Application.find(params[:id])
+     end
     
-   def index
-
-   if params[:search]
-    @applications = Application.search(params[:search]).order("created_at DESC")
-   end
-   end
-  # It returns the articles whose titles contain one or more words that form the query
-
-    # a method for viewing my purchases
-  
-    def show
-    @application = Application.find(params[:id])
-    end
-    def create 
+     def create 
         @application = Application.new(application_params)
         @application.save
-        
         @application.publisher_email  = current_user.email
         @application.save
         redirect_to @application
-    end
+     end
     
-    private
+     private
      def application_params
-    params.require(:application).permit(:appname, :description, :price)
+     params.require(:application).permit(:appname, :description, :price)
      end
      
 end
