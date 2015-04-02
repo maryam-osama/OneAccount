3c24a4b72bf3a8d2719e0827ca97c7fcf76c80ca
maikany <mai.kany62@gmail.com>
2015-04-02 13:34:45 +0000

    Some of the issues were fixed for code reviewing

--- SprintZero/config/application.rb
+++ SprintZero/config/application.rb
@@ -5,7 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
+config.generators.helper = false
 module Sprint0
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
