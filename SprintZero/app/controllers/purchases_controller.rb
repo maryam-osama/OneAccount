6dd5398383662ca65d013e3e56d81baf1aa6f0ad
maikany <mai.kany62@gmail.com>
2015-04-02 12:12:00 +0000

    documentation of viewing the apps purchased by the user was added

--- SprintZero/app/controllers/purchases_controller.rb
+++ SprintZero/app/controllers/purchases_controller.rb
@@ -1,10 +1,16 @@
 class PurchasesController < ApplicationController
-    
+     ##
+     # This is a class of purchases, it
+     # has method index which saves  
+     # all the application names purchased
+     # by the user who logged in a variable
+     # appnames by which all his purchases
+     # will be listed in the view.
     
       def index
   purchases = Purchase.where(user_email: current_user.email)
   @app_names = purchases.map{|p| Application.find(p.app_id) }
-    end
+      end
     
     
 end
