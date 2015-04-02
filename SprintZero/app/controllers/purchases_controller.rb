59bc11de0f46fca417cb56cfd54290bcf6e0a615
maikany <mai.kany62@gmail.com>
2015-04-02 16:57:53 +0000

    docoumentation is edited

--- SprintZero/app/controllers/purchases_controller.rb
+++ SprintZero/app/controllers/purchases_controller.rb
@@ -1,14 +1,13 @@
 class PurchasesController < ApplicationController
      ##
-     # This is a class of purchases, it
-     # has method index which saves  
-     # all the application names purchased
-     # by the user who logged in a variable
-     # appnames by which all his purchases
+     # This is a class of purchases,
+     # it has method index that saves all
+     # the application names purchased by
+     # the user who logged in.
+     # A variable appnames which saves all the user's purchases
      # will be listed in the view.
-    
-      def index
+     def index
   purchases = Purchase.where(user_email: current_user.email)
   @app_names = purchases.map { |p| Application.find(p.app_id) }
-      end
+     end
 end
