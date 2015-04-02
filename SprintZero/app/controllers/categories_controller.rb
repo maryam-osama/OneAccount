003e828b8982cd565ed3a126883babced056d382
rawan_abdelhalim <rawan.abdelhalim2@gmail.com>
2015-04-02 21:35:32 +0000

    #25 commented on create and new method-categories_controller

--- SprintZero/app/controllers/categories_controller.rb
+++ SprintZero/app/controllers/categories_controller.rb
@@ -1,10 +1,12 @@
 class CategoriesController < ApplicationController
-     
+    # defines the new action 
      def new
      end
        def index
     @categories = Category.all 
        end
+     # creates a new record with the recieved attribute, saves it in the model
+     ## redirects user to the list of categories
      def create
          @category = Category.new(params.require(:category).permit(:name))
           @category.save
