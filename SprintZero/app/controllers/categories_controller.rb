81344793aa9c9f87a74efcaf956506845dc00e9d
rawan_abdelhalim <rawan.abdelhalim2@gmail.com>
2015-04-02 22:22:02 +0000

    #25 updated commenting syntax- categories_controller

--- SprintZero/app/controllers/categories_controller.rb
+++ SprintZero/app/controllers/categories_controller.rb
@@ -1,12 +1,18 @@
+##
+# class that defines methods for perfoming CRUD operations on categories
 class CategoriesController < ApplicationController
+    ##
     # defines the new action 
      def new
      end
        def index
     @categories = Category.all 
        end
-     # creates a new record with the recieved attribute, saves it in the model
-     ## redirects user to the list of categories
+     ##  
+     # method creates a new record with the recieved attribute in the database
+     # and saves the record in the model Category
+     #
+     # it redirects user to the list of categories
      def create
          @category = Category.new(params.require(:category).permit(:name))
           @category.save
