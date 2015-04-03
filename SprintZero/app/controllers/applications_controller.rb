fd6e09bdb7310dea802a16e1c5e338a3575ccab6
khadijaibrahim <khadija.ibrahim.28@gmail.com>
2015-04-03 00:45:56 +0000

    i have updated the documentation

--- SprintZero/app/controllers/applications_controller.rb
+++ SprintZero/app/controllers/applications_controller.rb
@@ -1,24 +1,27 @@
- ## This is class of applications
+ ## 
+ #This is class of applications
 class ApplicationsController < ApplicationController
-    
     def new 
     end
     
-    ## This method lists all the publisher's app
-    # by  checking if the applications model have current_user_Email as a publisher email
-    # Then it loops on these applications
+    ## 
+    #This method lists all the publisher's app
+    #
+    #By  checking if the applications model have current_user_Email as a publisher email
+    # 
+    #Then it loops on these applications
     # and views the appname of this applications
     
     def myapps
-        
-        @applications = Application.where( publisher_email: current_user.email)
-    
+         @applications = Application.where( publisher_email: current_user.email)
     end
     
    def index
 
        if params[:search]
+       
        @applications = Application.search(params[:search]).order("created_at DESC")
+       
        end
    end
   # It returns the articles whose titles contain one or more words that form the query
@@ -31,16 +34,19 @@ class ApplicationsController < ApplicationController
     
     def create 
         @application = Application.new(application_params)
+        
         @application.save
         
         @application.publisher_email  = current_user.email
+        
         @application.save
+        
         redirect_to @application
     end
     
     private
      def application_params
-    params.require(:application).permit(:appname, :description, :price)
+         params.require(:application).permit(:appname, :description, :price)
      end
 end
     
