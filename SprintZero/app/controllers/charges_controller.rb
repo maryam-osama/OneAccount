eb63e3d62135a038dc1245e96fd4f3fe302cdb15
maikany <mai.kany62@gmail.com>
2015-04-03 20:22:27 +0000

    I updated my code style

--- SprintZero/app/controllers/charges_controller.rb
+++ SprintZero/app/controllers/charges_controller.rb
@@ -1,28 +1,27 @@
 class ChargesController < ApplicationController
-    def new; end
+  def new; end
 
-    def create
-  
+  def create
     # Amount in cents
     @amount = params[:number]   
     customer = Stripe::Customer.create(
     :email => 'example@stripe.com',
     :card  => params[:stripeToken]
   )
-  charge = Stripe::Charge.create(
+    charge = Stripe::Charge.create(
     :customer    => customer.id,
     :amount      =>  @amount ,
     :description => 'Rails Stripe customer',
     :currency    => 'usd'
   )
-   @purchase =Purchase.new
-   @purchase.user_email = current_user.email
-   @purchase.app_id =params[:application]
-   @purchase.save
-  rescue Stripe::CardError => e
-  flash[:error] = e.message
-  redirect_to charges_path(params[:number],params[:application])
-    end
+    @purchase =Purchase.new
+    @purchase.user_email = current_user.email
+    @purchase.app_id =params[:application]
+    @purchase.save
+    rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to charges_path(params[:number],params[:application])
+  end
     
 end
 
--- SprintZero/app/controllers/purchases_controller.rb
+++ SprintZero/app/controllers/purchases_controller.rb
@@ -5,9 +5,9 @@ class PurchasesController < ApplicationController
      # Saves all the application names purchased by
      # the user who logged in which
      # will be listed in the view.
-     def index
-       purchases = Purchase.where(user_email: current_user.email)
-       @app_names = purchases.map { |p| Application.find(p.app_id) }
-     end
+  def index
+    purchases = Purchase.where(user_email: current_user.email)
+    @app_names = purchases.map { |p| Application.find(p.app_id) }
+  end
      
 end
