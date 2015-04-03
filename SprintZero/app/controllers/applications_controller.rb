48f84deacf531dcc239f6ed8fdf4da1297533506
Basma Elgeziry <b.geziry@robone.net>
2015-04-03 14:31:04 +0000

    finished documentation and review

--- SprintZero/app/controllers/applications_controller.rb
+++ SprintZero/app/controllers/applications_controller.rb
@@ -1,46 +1,41 @@
 class ApplicationsController < ApplicationController
-    
-    
     def new 
     end
     
    def index
-
-   if params[:search]
-    @applications = Application.search(params[:search]).order("created_at DESC")
+     if params[:search]
+     @applications = Application.search(params[:search]).order("created_at DESC")
+     end
    end
-  end
   # It returns the articles whose titles contain one or more words that form the query
 
-    
-    
     def show
-    @application = Application.find(params[:id])
+      @application = Application.find(params[:id])
     end
     
     def create 
-        @application = Application.new(application_params)
-        @application.save
-        
-        @application.publisher_email  = current_user.email
-        @application.save
-        redirect_to @application
+     @application = Application.new(application_params)
+     @application.save
+     @application.publisher_email  = current_user.email
+     @application.save
+     redirect_to @application
     end
     
    def destroy
-      
-   @application = Application.find(params[:id])
-   @application.destroy
-   @application.save
-   redirect_to welcome_homepage_url
+     @application = Application.find(params[:id])
+     @application.destroy
+     @application.save
+     redirect_to welcome_homepage_url
    end 
- 
+   # Method destroy linked to the delete method and functions to go back to homepage which delete the app
    
-    private
+   private
      def application_params
-    params.require(:application).permit(:appname, :description, :price)
+       params.require(:application).permit(:appname, :description, :price)
      end
+     
 end
+    
     
     
 
--- SprintZero/app/controllers/requests_controller.rb
+++ SprintZero/app/controllers/requests_controller.rb
@@ -1,44 +1,37 @@
 class RequestsController < ApplicationController
+  def index
+    @requests=Request.all
+  end
     
+  def show
+    @request=Request.find(params[:id])
+  end
     
-    def index
-        @requests=Request.all
-    end
-    
-    def show
-        @request=Request.find(params[:id])
-    end
-    
-    def new
+  def new
     @request = Request.new
-    end
+  end
    
-    def create
+  def create
     @request = Request.new(requests_params)
     @request.save
     redirect_to welcome_homepage_url
-    end
-   
-  
+  end
+    
   def destroy
-      
-   @request = Request.find(params[:id])
-   @user = User.where(:email => @request.user_email).take
-   
-   @request.destroy
-   @request.save
-  
-   if params[:number] == "1"
-   @user.publisher = true
-   @user.save
-   end
-   redirect_to requests_path
-   
+    @request = Request.find(params[:id])
+    @user = User.where(:email => @request.user_email).take
+    @request.destroy
+    @request.save
+      if params[:number] == "1"
+      @user.publisher = true
+      @user.save
+      end
+    redirect_to requests_path
   end
 
   private
-  def requests_params
-    params.require(:request).permit(:reason, :user_email)
-  end
+    def requests_params
+      params.require(:request).permit(:reason, :user_email)
+    end
  
 end
