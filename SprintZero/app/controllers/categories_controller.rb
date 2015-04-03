2615bce304b9b9ccf1e9b12f57e0c8f1b75019ad
samaasallam <samaa.sallam@gmail.com>
2015-04-03 00:16:40 +0000

    #18 Did documentation to the category controller

--- SprintZero/app/controllers/categories_controller.rb
+++ SprintZero/app/controllers/categories_controller.rb
@@ -2,6 +2,8 @@ class CategoriesController < ApplicationController
      
      def new
      end
+     ##
+     # Gets all availble categories and saves it in a variable to be displayed later 
        def index
     @categories = Category.all 
        end
@@ -10,6 +12,8 @@ class CategoriesController < ApplicationController
           @category.save
   redirect_to categories_path
      end
+    ## 
+    # Finds a specific category by it's id and saves it in a variable 
      def show
          @category = Category.find(params[:id])
      end
