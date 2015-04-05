965256d34c2ec854f58311493d5d212137da867e
samaasallam <samaa.sallam@gmail.com>
2015-04-05 11:00:09 +0000

    #18 The charges and reuquests controllers are now updated

--- SprintZero/app/controllers/charges_controller.rb
+++ SprintZero/app/controllers/charges_controller.rb
@@ -1,5 +1,5 @@
 class ChargesController < ApplicationController
-  def new;end
+  def new; end
    
   def create
     # Amount in cents
@@ -7,14 +7,14 @@ class ChargesController < ApplicationController
     customer = Stripe::Customer.create(
     :email => 'example@stripe.com',
     :card  => params[:stripeToken]
-     )
+  )
     charge = Stripe::Charge.create(
     :customer    => customer.id,
     :amount      =>  @amount ,
     :description => 'Rails Stripe customer',
     :currency    => 'usd'
-     )
-    @purchase =Purchase.new
+  )
+    @purchase = Purchase.new
     @purchase.user_email = current_user.email
     @purchase.app_id =params[:application]
     @purchase.save
