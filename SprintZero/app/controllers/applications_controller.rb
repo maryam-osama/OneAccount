9490b92ca9abcac1f262d3da3e03831a36388036
maryam_osama <maryam.osama06@gmail.com>
2015-04-05 17:03:52 +0000

    Errors Fixed

--- SprintZero/app/controllers/applications_controller.rb
+++ SprintZero/app/controllers/applications_controller.rb
@@ -3,6 +3,7 @@
 class ApplicationsController < ApplicationController
 
     def new
+    @application = Application.new
     @categories = Category.all  
     end
 
@@ -42,15 +43,17 @@ class ApplicationsController < ApplicationController
   def create 
     @application = Application.new(application_params)
     @application.publisher_email  = current_user.email
-    @application.save
+    
+    if @application.save
     @notification = Notification.new
     @notification.user_email = current_user.email
     @notification.appname = @application.appname
     @notification.app_id = @application.id
     @notification.save
     redirect_to @application
-    
-    
+    else
+    render 'new'
+    end
    
   end
     
@@ -71,7 +74,7 @@ class ApplicationsController < ApplicationController
   
    private
    def application_params
-       params.require(:application).permit(:appname, :description, :price)
+            params.require(:application).permit(:appname, :description, :price, :category_name)
    end
      
 end
