d0616877dde59eca1dedfa40a5389295f095e6c9
maryam_osama <maryam.osama06@gmail.com>
2015-04-05 19:23:34 +0000

    Some Changes

--- SprintZero/db/schema.rb
+++ SprintZero/db/schema.rb
@@ -11,11 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20150320125146) do
-=======
 ActiveRecord::Schema.define(version: 20150331090102) do
->>>>>>> Test
+
 
   create_table "applications", force: true do |t|
     t.string   "appname"
@@ -24,8 +21,6 @@ ActiveRecord::Schema.define(version: 20150331090102) do
     t.string   "publisher_email"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-<<<<<<< HEAD
-=======
     t.string   "category_name"
   end
 
@@ -33,7 +28,7 @@ ActiveRecord::Schema.define(version: 20150331090102) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
->>>>>>> Test
+
   end
 
   create_table "notifications", force: true do |t|
