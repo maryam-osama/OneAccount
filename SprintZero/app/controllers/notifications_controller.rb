3ccf1d23697e58557266c27dcabb2f261873ee83
basmaelgeziry <basma.elgeziry38@gmail.com>
2015-04-07 07:12:38 +0000

    updated the review

--- SprintZero/app/controllers/notifications_controller.rb
+++ SprintZero/app/controllers/notifications_controller.rb
@@ -1,37 +1,38 @@
 ##
 # Lists,Shows and Creates Notifications
 class NotificationsController < ApplicationController
-  ##   
+
+  ##
   # Returns all the Notifications stored in the database
   def index
     @notification = Notification.all
   end
-    
-  ##    
-  # Returns a specific Notification from the database by supplying the id  
+
+  ##
+  # Returns a specific Notification from the database by supplying the id
   def show
     @notification = Notification.find(params[:id])
   end
 
   ##
-  # Creates new instance of Notification model    
+  # Creates new instance of Notification model
   def new
     @notification = Notification.new
   end
-    
-  ##   
-  # Assigns paramters to a newly created Notification and saves it in the database   
+
+  ##
+  # Assigns paramters to a newly created Notification and saves it in the database
   def create
     @notification = Notification.new(notifications_params)
     @notification.save
   end
-    
-  ##    
-  # Specifies the notifications_params to be user_email , appname and app_id  
+
+  ##
+  # Specifies the notifications_params to be user_email , appname and app_id
   private
   def notifications_params
     params.require(:notification).permit(:user_email, :appname, :app_id)
   end
-   
+
 end
 
