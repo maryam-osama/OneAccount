a71b5765b3b896122cd1e167b459db11649312ed
fadykesbra <foreverreigns@live.com>
2015-04-08 18:48:56 +0000

    #19 worked on the homepage and my purchases page

--- SprintZero/app/models/application.rb
+++ SprintZero/app/models/application.rb
@@ -1,9 +1,9 @@
 class Application < ActiveRecord::Base
-   validates :appname, presence: true
+  validates :appname, presence: true
                   
-   validates :description, presence: true
+  validates :description, presence: true
                    
-   validates :price, numericality: true, presence:true
+  validates :price, numericality: true, presence:true
                     
   def self.search(query)
     where("appname like ?", "%#{query}%") 
