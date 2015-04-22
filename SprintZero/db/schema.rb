40e7ffdd160571224f892a126c8c61e6b92e05ee
norinehesham <norine.badr94@gmail.com>
2015-04-22 18:10:11 +0000

    #36 i added the rate to the show of the app

--- SprintZero/db/schema.rb
+++ SprintZero/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150331090102) do
+ActiveRecord::Schema.define(version: 20150421131408) do
 
   create_table "applications", force: true do |t|
     t.string   "appname"
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 20150331090102) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "category_name"
+  end
+
+  create_table "average_caches", force: true do |t|
+    t.integer  "rater_id"
+    t.integer  "rateable_id"
+    t.string   "rateable_type"
+    t.float    "avg",           null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "categories", force: true do |t|
@@ -37,12 +46,45 @@ ActiveRecord::Schema.define(version: 20150331090102) do
     t.integer  "app_id"
   end
 
+  create_table "overall_averages", force: true do |t|
+    t.integer  "rateable_id"
+    t.string   "rateable_type"
+    t.float    "overall_avg",   null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "purchases", force: true do |t|
     t.string   "user_email"
     t.integer  "app_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "rates", force: true do |t|
+    t.integer  "rater_id"
+    t.integer  "rateable_id"
+    t.string   "rateable_type"
+    t.float    "stars",         null: false
+    t.string   "dimension"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "rates", ["rateable_id", "rateable_type"], name: "index_rates_on_rateable_id_and_rateable_type"
+  add_index "rates", ["rater_id"], name: "index_rates_on_rater_id"
+
+  create_table "rating_caches", force: true do |t|
+    t.integer  "cacheable_id"
+    t.string   "cacheable_type"
+    t.float    "avg",            null: false
+    t.integer  "qty",            null: false
+    t.string   "dimension"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "rating_caches", ["cacheable_id", "cacheable_type"], name: "index_rating_caches_on_cacheable_id_and_cacheable_type"
 
   create_table "requests", force: true do |t|
     t.string   "user_email"
