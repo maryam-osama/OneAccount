7929579e42c896eb2767a0f447572a5bbdb456d9
khadijaibrahim <khadija.ibrahim.28@gmail.com>
2015-04-22 20:47:45 +0000

    #44 I have added the comments

---
+++ SprintZero/app/controllers/comments_controller.rb
@@ -0,0 +1,17 @@
+class CommentsController < ApplicationController
+  ##
+  #This method creates and saves a comment
+  def create
+    @application = Application.find(params[:application_id])
+    @comment = @application.comments.create(comment_params)
+    redirect_to application_path(@application)
+    @comment.commenter = current_user.email
+    @comment.save
+  end
+ 
+  private
+    def comment_params
+      params.require(:comment).permit(:commenter, :body)
+    end
+
+end
--- SprintZero/app/models/application.rb
+++ SprintZero/app/models/application.rb
@@ -1,4 +1,5 @@
 class Application < ActiveRecord::Base
+  has_many :comments
   validates :appname, presence: true
   validates :description, presence: true
   validates :price, numericality: true, presence:true
