50e1833e6c2edcbb5548c829e53e97fcd2bf5559
Basma Elgeziry <b.geziry@robone.net>
2015-04-24 11:44:06 +0000

    #37 the destroy method for the comment has been added

--- SprintZero/app/models/application.rb
+++ SprintZero/app/models/application.rb
@@ -3,7 +3,10 @@ class Application < ActiveRecord::Base
   validates :appname, presence: true
   validates :description, presence: true
   validates :price, numericality: true, presence:true
-                 
+  has_many :comments, dependent: :destroy
+  validates :title, presence: true,
+                    length: { minimum: 5 }
+
   def self.search(query)
     where("appname like ?", "%#{query}%") 
   end
