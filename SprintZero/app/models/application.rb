6f800c61241aab55b98b4736bc279afb7482ce5a
norinehesham <norine.badr94@gmail.com>
2015-04-25 13:03:51 +0000

    #36 i edted the show of the rate and added code according to the convention

--- SprintZero/app/models/application.rb
+++ SprintZero/app/models/application.rb
@@ -2,9 +2,8 @@ class Application < ActiveRecord::Base
   validates :appname, presence: true
   validates :description, presence: true
   validates :price, numericality: true, presence:true
-  ratyrate_rateable 'Rate', 'original_score'
+  ratyrate_rateable 'Rate'
   
-                 
   def self.search(query)
     where("appname like ?", "%#{query}%") 
   end
