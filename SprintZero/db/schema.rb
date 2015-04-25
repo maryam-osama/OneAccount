4b10c9ef197a350f4882bac9abd0060b5a43abda
samaasallam <samaa.sallam@gmail.com>
2015-04-25 13:11:19 +0000

    #40 The Code is now ready and documentations is written

--- SprintZero/db/schema.rb
+++ SprintZero/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150420083421) do
+ActiveRecord::Schema.define(version: 20150425103500) do
 
   create_table "applications", force: true do |t|
     t.string   "appname"
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(version: 20150420083421) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "category_name"
-    t.string   "url"
     t.string   "appstore_url"
     t.string   "playstore_url"
     t.string   "windows_url"
