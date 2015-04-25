dc9b3c3a4cf29b60ef9363b130a4ba6e6fc0ce63
norinehesham <norine.badr94@gmail.com>
2015-04-25 13:33:51 +0000

    #36 i added documentation convention

--- SprintZero/app/models/rate.rb
+++ SprintZero/app/models/rate.rb
@@ -1,7 +1,7 @@
 ## 
-#Author: Norine Hesham
+# Author: Norine Hesham
 #
-#This is class of rate
+# This is class of rate
 class Rate < ActiveRecord::Base
   belongs_to :rater, :class_name => "User"
   belongs_to :rateable, :polymorphic => true
