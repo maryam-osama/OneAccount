cb749a082f63478d309d8313eb1c4705ceac3a62
basmaelgeziry <basma.elgeziry38@gmail.com>
2015-04-25 18:00:37 +0000

    #37  I updated my delete button

--- SprintZero/app/models/application.rb
+++ SprintZero/app/models/application.rb
@@ -4,8 +4,7 @@ class Application < ActiveRecord::Base
   validates :description, presence: true
   validates :price, numericality: true, presence:true
   has_many :comments, dependent: :destroy
-  validates :title, presence: true,
-                    length: { minimum: 5 }
+  
 
   def self.search(query)
     where("appname like ?", "%#{query}%") 
