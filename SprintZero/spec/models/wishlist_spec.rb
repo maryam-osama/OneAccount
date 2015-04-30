db71b5256062e45d9a1883591744a70b5f828336
rawan_abdelhalim <rawan.abdelhalim2@gmail.com>
2015-04-30 23:09:13 +0000

    #45 added testing

--- SprintZero/spec/models/wishlist_spec.rb
+++ SprintZero/spec/models/wishlist_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Wishlist, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    
+ it "is valid with a email , app_id , user_id " do
+      wishlist = Wishlist.new(
+         email: 'rawan@gmail.com',
+         app_id: 1,
+         user_id: 1 )
+        expect(wishlist).to be_valid
+     
+   end
+ 
+ 
+ 
 end
