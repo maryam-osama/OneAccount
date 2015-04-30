7ceb25922e30b2ab6abe71b53c2c9057990e2f23
maryam_osama <maryam.osama06@gmail.com>
2015-04-30 17:34:50 +0000

    Fixed Some errors

--- SprintZero/spec/rails_helper.rb
+++ SprintZero/spec/rails_helper.rb
@@ -5,8 +5,7 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
 
 # Add this to load Capybara integration:
-require 'capybara/rspec'
-require 'capybara/rails'
+
 
 # Add additional requires below this line. Rails is not loaded until this point!
 
